28a4bb46cd7aa5a59b87a935511424f550385a4f
= <maixiemm@gmail.com>
2018-10-04 20:47:27 -0500

    update functions file

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function my_scripts(){
-	wp_enqueue_style('my_style', get_stylesheet_uri() . 'style.css');
+	wp_enqueue_style('my_style', get_stylesheet_uri());
 }
 	add_action('wp_enqueue_scripts', 'my_scripts');
 ?>
